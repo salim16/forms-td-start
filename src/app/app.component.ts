48062333c06684cb459e5433647d172c9c2e250a
Mohd Salim <mohd.salim@soprasteria.com>
2019-08-13 16:09:57 +0530

    TD: Resetting Forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -57,5 +57,7 @@ export class AppComponent {
     this.user.secretQuestion = this.signUpForm.value.secret;
     this.user.answer = this.signUpForm.value.questionAnswer;
     this.user.gender = this.signUpForm.value.gender;
+
+    this.signUpForm.reset();
   }
 }
